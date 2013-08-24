c091788bc0649e5820ef7b2db8e6d34aae5ac9f0
Dave Johnson <snoopdave@gmail.com>
2013-08-24 16:47:26 -0400

    initial commit

---
+++ src/main/java/snoopware/api/RoutingFilter.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2013 David M. Johnson (snoopdave@gmail.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package snoopware.api;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+@WebFilter("/*")
+public class RoutingFilter implements Filter {
+
+	private String angularRoutes[] = {
+		"login",	
+		"page1",	
+		"page2"	
+	};
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
+		throws IOException, ServletException {
+
+		HttpServletRequest req = (HttpServletRequest)request;
+		System.out.println(req.getServletPath());
+
+		for (String angularRoute : angularRoutes) {
+			if (req.getServletPath().endsWith(angularRoute)) {
+				RequestDispatcher rd = req.getRequestDispatcher("/index.html");
+				System.out.println("Dispatching " + req.getServletPath() + " to " + "/index.html");
+				rd.forward(request, response);
+				return;
+			}
+		}
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {}
+}
---
+++ src/main/webapp/js/controllers.js
@@ -0,0 +1,68 @@
+
+var rootPath = "/angular-usergrid";
+
+client = new Usergrid.Client({
+	orgName: "snoopdave",
+	appName: "volda",
+	logging: true,
+	buildCurl: true,
+	URI: "http://localhost:8080/" + rootPath + "/api"
+});
+
+var appModule = angular.module("appModule", ["ui.bootstrap"]);
+appModule.config(function($routeProvider, $locationProvider) {
+	$locationProvider.html5Mode(true);
+	$routeProvider
+		.when(rootPath + "/login", {
+			controller:LoginController, templateUrl:"login.html"})
+		.when(rootPath + "/", {
+			controller:Page1Controller, templateUrl:"page1.html"})
+		.when(rootPath + "/page2", { 
+			controller:Page2Controller, templateUrl:"page2.html"})
+		.otherwise({"redirectTo":rootPath});
+});
+
+function LoginController($scope, $location) {
+	$scope.username = "";
+	$scope.password = "";
+	$scope.response = "";
+	$scope.alerts = [];
+	$scope.login = function(uname, pword) {
+		client.login(uname, pword, function(err, data) {
+			if (err) {
+				$scope.alerts.push({type:"error", msg:"Login failed, please try again..."});
+			} else {
+				$scope.alerts.push({type:"success", msg:"Login success. Directing you to page #1"});
+				$location.path(rootPath + "/page1");
+			}
+			$scope.$apply();
+		});
+	};
+	$scope.closeAlert = function(index) {
+    	$scope.alerts.splice(index, 1);
+	};
+}
+
+function Page1Controller($scope, $location) {
+	if (!client.getToken()) {
+		$location.path(rootPath + "/login");
+	} else {
+		$scope.message = "Welcome to page #1";
+	}
+	$scope.logout = function() { 
+		client.logout(); 
+		$location.path(rootPath + "/login");
+	};
+}
+
+function Page2Controller($scope, $location) {
+	if (!client.getToken()) {
+		$location.path(rootPath + "/login");
+	} else {
+		$scope.message = "Welcome to page #2";
+	}
+	$scope.logout = function() { 
+		client.logout(); 
+		$location.path(rootPath + "/login");
+	};
+}
