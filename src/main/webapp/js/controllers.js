34d843b96aeb76f146b761caed7bcafb263f42b4
Dave Johnson <snoopdave@gmail.com>
2013-08-25 11:07:27 -0400

    simple password verification

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -15,42 +15,43 @@ appModule.config(function($routeProvider, $locationProvider) {
 			controller:Page1Controller, templateUrl:"page1.html"})
 		.when(rootPath + "/page1", {
 			controller:Page1Controller, templateUrl:"page1.html"})
-		.when(rootPath + "/page2", { 
+		.when(rootPath + "/page2", {
 			controller:Page2Controller, templateUrl:"page2.html"})
 		.when(rootPath + "/login", {
 			controller:LoginController, templateUrl:"login.html"})
-		.when(rootPath + "/regsiter", {
-			controller:LoginController, templateUrl:"register.html"})
+		.when(rootPath + "/register", {
+			controller:RegistrationController, templateUrl:"register.html"})
 		.otherwise({"redirectTo":rootPath});
 });
 
 function LoginController($scope, $location) {
 	$scope.username = "";
 	$scope.password = "";
-	$scope.response = "";
+	$scope.password_verify = "";
 	$scope.alerts = [];
 	$scope.login = function(uname, pword) {
 		client.login(uname, pword, function(err, data) {
 			if (err) {
-				$scope.alerts.push({type:"error", msg:"Login failed, please try again..."});
+				$scope.alerts.push({type: "error", msg: "Login failed, please try again..."});
 			} else {
-				$scope.alerts.push({type:"success", msg:"Login success. Directing you to page #1"});
+				$scope.alerts.push({type: "success", msg: "Login success. Directing you to page #1"});
 				$location.path(rootPath + "/page1");
 			}
 			$scope.$apply();
 		});
 	};
 	$scope.closeAlert = function(index) {
-    	$scope.alerts.splice(index, 1);
+		$scope.alerts.splice(index, 1);
 	};
 }
 
 function RegistrationController($scope, $location) {
 	$scope.username = "";
+	$scope.email = "";
 	$scope.password = "";
-	$scope.response = "";
+	$scope.password_verify = "";
 	$scope.alerts = [];
-	$scope.register = function(uname, pword) {
+	$scope.register = function(username, email, password) {
 		client.login(uname, pword, function(err, data) {
 			if (err) {
 				$scope.alerts.push({type:"error", msg:"Login failed, please try again..."});
@@ -62,9 +63,26 @@ function RegistrationController($scope, $location) {
 		});
 	};
 	$scope.closeAlert = function(index) {
-    	$scope.alerts.splice(index, 1);
+		$scope.alerts.splice(index, 1);
+	};
+	$scope.validate = function() {
+		return false;
 	};
 }
+
+// Based on code from http://plnkr.co/edit/DIq3i6YHnPc9EJzd21b3?p=preview 
+appModule.directive('match', function($parse) {
+	return {
+		require: 'ngModel',
+		link: function(scope, elem, attrs, ctrl) {
+			scope.$watch(function() {
+				return $parse(attrs.match)(scope) === ctrl.$modelValue;
+			}, function(currentValue) {
+				ctrl.$setValidity('mismatch', currentValue);
+			});
+		}
+	};
+});
 
 function Page1Controller($scope, $location) {
 	if (!client.getToken()) {
@@ -72,8 +90,8 @@ function Page1Controller($scope, $location) {
 	} else {
 		$scope.message = "Welcome to page #1";
 	}
-	$scope.logout = function() { 
-		client.logout(); 
+	$scope.logout = function() {
+		client.logout();
 		$location.path(rootPath + "/login");
 	};
 }
@@ -84,8 +102,8 @@ function Page2Controller($scope, $location) {
 	} else {
 		$scope.message = "Welcome to page #2";
 	}
-	$scope.logout = function() { 
-		client.logout(); 
+	$scope.logout = function() {
+		client.logout();
 		$location.path(rootPath + "/login");
 	};
 }
