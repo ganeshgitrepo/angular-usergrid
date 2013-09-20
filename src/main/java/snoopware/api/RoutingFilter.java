5b29d1803ddfc84aca62f97fef977882e8c78a41
Dave Johnson <dmjohnson@apigee.com>
2013-09-20 17:35:42 -0400

    removing the evil that is tabs

--- src/main/java/snoopware/api/RoutingFilter.java
+++ src/main/java/snoopware/api/RoutingFilter.java
@@ -29,35 +29,35 @@ import javax.servlet.http.HttpServletRequest;
 
 @WebFilter("/*")
 public class RoutingFilter implements Filter {
-	private static final Logger log = Logger.getLogger(RoutingFilter.class.getName());
+  private static final Logger log = Logger.getLogger(RoutingFilter.class.getName());
 
-	private String angularRoutes[] = {
-		"login",	
-		"register",	
-		"page1",	
-		"page2"	
-	};
+  private String angularRoutes[] = {
+    "login",  
+    "register",  
+    "page1",  
+    "page2"  
+  };
 
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {}
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {}
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
-		throws IOException, ServletException {
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
+    throws IOException, ServletException {
 
-		HttpServletRequest req = (HttpServletRequest)request;
+    HttpServletRequest req = (HttpServletRequest)request;
 
-		for (String angularRoute : angularRoutes) {
-			if (req.getServletPath().endsWith(angularRoute)) {
-				RequestDispatcher rd = req.getRequestDispatcher("/index.html");
-				//log.log(Level.INFO,"Dispatching {0} to /index.html", req.getServletPath());
-				rd.forward(request, response);
-				return;
-			}
-		}
-		chain.doFilter(request, response);
-	}
+    for (String angularRoute : angularRoutes) {
+      if (req.getServletPath().endsWith(angularRoute)) {
+        RequestDispatcher rd = req.getRequestDispatcher("/index.html");
+        //log.log(Level.INFO,"Dispatching {0} to /index.html", req.getServletPath());
+        rd.forward(request, response);
+        return;
+      }
+    }
+    chain.doFilter(request, response);
+  }
 
-	@Override
-	public void destroy() {}
+  @Override
+  public void destroy() {}
 }
--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -1,76 +1,76 @@
 var rootPath = "/angular-usergrid";
 client = new Usergrid.Client({
-	orgName: "test-organization",
-	appName: "test-app",
-	logging: true,
-	buildCurl: true,
-	URI: "http://localhost:8080" + rootPath + "/api"
+  orgName: "snoopdave",
+  appName: "volda",
+  logging: true,
+  buildCurl: true,
+  URI: "http://localhost:8080" + rootPath + "/api"
 });
 
 var appModule = angular.module("appModule", ["ui.bootstrap"]);
 appModule.config(function($routeProvider, $locationProvider) {
-	$locationProvider.html5Mode(true);
-	$routeProvider
-		.when(rootPath + "/", {
-			controller:Page1Controller, templateUrl:"page1.html"})
-		.when(rootPath + "/page1", {
-			controller:Page1Controller, templateUrl:"page1.html"})
-		.when(rootPath + "/page2", {
-			controller:Page2Controller, templateUrl:"page2.html"})
-		.when(rootPath + "/login", {
-			controller:LoginController, templateUrl:"login.html"})
-		.when(rootPath + "/register", {
-			controller:RegistrationController, templateUrl:"register.html"})
-		.when(rootPath + "/registered", {
-			controller:RegisteredController, templateUrl:"registered.html"})
-		.otherwise({"redirectTo":rootPath});
+  $locationProvider.html5Mode(true);
+  $routeProvider
+    .when(rootPath + "/", {
+      controller:Page1Controller, templateUrl:"page1.html"})
+    .when(rootPath + "/page1", {
+      controller:Page1Controller, templateUrl:"page1.html"})
+    .when(rootPath + "/page2", {
+      controller:Page2Controller, templateUrl:"page2.html"})
+    .when(rootPath + "/login", {
+      controller:LoginController, templateUrl:"login.html"})
+    .when(rootPath + "/register", {
+      controller:RegistrationController, templateUrl:"register.html"})
+    .when(rootPath + "/registered", {
+      controller:RegisteredController, templateUrl:"registered.html"})
+    .otherwise({"redirectTo":rootPath});
 });
 
 function LoginController($scope, $location) {
-	$scope.username = "";
-	$scope.password = "";
-	$scope.password_verify = "";
-	$scope.alerts = [];
-	$scope.login = function(uname, pword) {
-		client.login(uname, pword, function(err, data) {
-			if (err) {
-				$scope.alerts.push({type: "error", 
-					msg: "Login failed, please try again..."});
-			} else {
-				$scope.alerts.push({type: "success", 
-					msg: "Login success. Directing you to page #1"});
-				$location.path(rootPath + "/page1");
-			}
-			$scope.$apply();
-		});
-	};
-	$scope.closeAlert = function(index) {
-		$scope.alerts.splice(index, 1);
-	};
+  $scope.username = "";
+  $scope.password = "";
+  $scope.password_verify = "";
+  $scope.alerts = [];
+  $scope.login = function(uname, pword) {
+    client.login(uname, pword, function(err, data) {
+      if (err) {
+        $scope.alerts.push({type: "error", 
+          msg: "Login failed, please try again..."});
+      } else {
+        $scope.alerts.push({type: "success", 
+          msg: "Login success. Directing you to page #1"});
+        $location.path(rootPath + "/page1");
+      }
+      $scope.$apply();
+    });
+  };
+  $scope.closeAlert = function(index) {
+    $scope.alerts.splice(index, 1);
+  };
 }
 
 function RegistrationController($scope, $location) {
-	$scope.username = "";
-	$scope.email = "";
-	$scope.password = "";
-	$scope.password_verify = "";
-	$scope.alerts = [];
-	$scope.register = function(username, password, email) {
-		client.signup(username, password, email, null, function(err, data) {
-			if (err) {
-				$scope.alerts.push({type:"error", 
-					msg:"Registration failed, please try again..."});
-			} else {
-				$scope.alerts.push({type:"success", 
-					msg:"Registrsation success. Directing you to login page."});
-				$location.path(rootPath + "/page1");
-			}
-			$scope.$apply();
-		});
-	};
-	$scope.closeAlert = function(index) {
-		$scope.alerts.splice(index, 1);
-	};
+  $scope.username = "";
+  $scope.email = "";
+  $scope.password = "";
+  $scope.password_verify = "";
+  $scope.alerts = [];
+  $scope.register = function(username, password, email) {
+    client.signup(username, password, email, null, function(err, data) {
+      if (err) {
+        $scope.alerts.push({type:"error", 
+          msg:"Registration failed, please try again..."});
+      } else {
+        $scope.alerts.push({type:"success", 
+          msg:"Registrsation success. Directing you to login page."});
+        $location.path(rootPath + "/page1");
+      }
+      $scope.$apply();
+    });
+  };
+  $scope.closeAlert = function(index) {
+    $scope.alerts.splice(index, 1);
+  };
 }
 
 function RegisteredController($scope, $location) {
@@ -78,81 +78,81 @@ function RegisteredController($scope, $location) {
 
 // Based on code from http://plnkr.co/edit/DIq3i6YHnPc9EJzd21b3?p=preview 
 appModule.directive('auMatch', function($parse) {
-	return {
-		require: 'ngModel',
-		link: function(scope, elem, attrs, ctrl) {
-			ctrl.$setValidity('match', false);
-			scope.$watch(function() {
-				return $parse(attrs.auMatch)(scope) === ctrl.$modelValue;
-			}, function(currentValue) {
-				ctrl.$setValidity('match', currentValue);
-			});
-		}
-	};
+  return {
+    require: 'ngModel',
+    link: function(scope, elem, attrs, ctrl) {
+      ctrl.$setValidity('match', false);
+      scope.$watch(function() {
+        return $parse(attrs.auMatch)(scope) === ctrl.$modelValue;
+      }, function(currentValue) {
+        ctrl.$setValidity('match', currentValue);
+      });
+    }
+  };
 });
 
 appModule.directive('auUniqueUsername', function() {
-	return {
-		require: 'ngModel',
-		link: function(scope, elem, attrs, ctrl) {
-			ctrl.$setValidity("unique", false);
-			elem.keyup(function(evt) {
-				var options = { type:"user", name: ctrl.$modelValue };
-				if (ctrl.$modelValue && ctrl.$modelValue.trim().length > 0) {
-					client.getEntity(options, function(err, data) {
-						ctrl.$setValidity("unique", err); // error means unique
-						scope.$apply();
-					});
-				} else {
-					ctrl.$setValidity("unique", false); // consider empty string non unique
-					scope.$apply();
-				}
-			});
-		}
-	};
+  return {
+    require: 'ngModel',
+    link: function(scope, elem, attrs, ctrl) {
+      ctrl.$setValidity("unique", false);
+      elem.keyup(function(evt) {
+        var options = { type:"user", name: ctrl.$modelValue };
+        if (ctrl.$modelValue && ctrl.$modelValue.trim().length > 0) {
+          client.getEntity(options, function(err, data) {
+            ctrl.$setValidity("unique", err); // error means unique
+            scope.$apply();
+          });
+        } else {
+          ctrl.$setValidity("unique", false); // consider empty string non unique
+          scope.$apply();
+        }
+      });
+    }
+  };
 });
 
 appModule.directive('auUniqueEmail', function() {
-	return {
-		require: 'ngModel',
-		link: function(scope, elem, attrs, ctrl) {
-			ctrl.$setValidity("unique", false);
-			elem.keyup(function(evt) {
-				var options = { type:"user", email: ctrl.$modelValue, name: ctrl.$modelValue };
-				if (ctrl.$modelValue && ctrl.$modelValue.trim().length > 0) {
-					client.getEntity(options, function(err, data) {
-						ctrl.$setValidity("unique", err); // error means unique
-						scope.$apply();
-					});
-				} else {
-					ctrl.$setValidity("unique", false); // consider empty string non unique
-					scope.$apply();
-				}
-			});
-		}
-	};
+  return {
+    require: 'ngModel',
+    link: function(scope, elem, attrs, ctrl) {
+      ctrl.$setValidity("unique", false);
+      elem.keyup(function(evt) {
+        var options = { type:"user", email: ctrl.$modelValue, name: ctrl.$modelValue };
+        if (ctrl.$modelValue && ctrl.$modelValue.trim().length > 0) {
+          client.getEntity(options, function(err, data) {
+            ctrl.$setValidity("unique", err); // error means unique
+            scope.$apply();
+          });
+        } else {
+          ctrl.$setValidity("unique", false); // consider empty string non unique
+          scope.$apply();
+        }
+      });
+    }
+  };
 });
 
 function Page1Controller($scope, $location) {
-	if (!client.getToken()) {
-		$location.path(rootPath + "/login");
-	} else {
-		$scope.message = "Welcome to page #1";
-	}
-	$scope.logout = function() {
-		client.logout();
-		$location.path(rootPath + "/login");
-	};
+  if (!client.getToken()) {
+    $location.path(rootPath + "/login");
+  } else {
+    $scope.message = "Welcome to page #1";
+  }
+  $scope.logout = function() {
+    client.logout();
+    $location.path(rootPath + "/login");
+  };
 }
 
 function Page2Controller($scope, $location) {
-	if (!client.getToken()) {
-		$location.path(rootPath + "/login");
-	} else {
-		$scope.message = "Welcome to page #2";
-	}
-	$scope.logout = function() {
-		client.logout();
-		$location.path(rootPath + "/login");
-	};
+  if (!client.getToken()) {
+    $location.path(rootPath + "/login");
+  } else {
+    $scope.message = "Welcome to page #2";
+  }
+  $scope.logout = function() {
+    client.logout();
+    $location.path(rootPath + "/login");
+  };
 }
